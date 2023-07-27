ec4ff3dacf0950bb716845d704e0c9245471374f
BRYRB <bryanopew@gmail.com>
2023-07-27 19:19:48 +0900

    [ETC] : change project name

--- ios/ArtVentureTests/ArtVentureTests.m
+++ ios/ArtVentureTests/ArtVentureTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ArtusTests : XCTestCase
+@interface ArtVentureTests : XCTestCase
 
 @end
 
-@implementation ArtusTests
+@implementation ArtVentureTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
